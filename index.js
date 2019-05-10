494498e8dd56fb28924885efbd71231a6acb8e5f
Rafael Cerri <rafaelbcerri@hotmail.com>
2019-05-10 14:10:49 -0300

    Feedback

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
-function cpf(cpfString) {
-    let cpf = cpfString.split('');
-    if (cpf.length < 11 || cpf.length > 11 || cpf.every(digit => digit === cpf[0])) {
-        return false;
-    } else if (digitValidator(cpf)) {
-        return true;
-    } else {
-        return false;
-    }   
+function cpfValidator(cpfString) {
+  const cpfArray = cpfString.split('');
+  const isCpfInvalid = cpfArray.length < 11 || cpfArray.length > 11 || cpfArray.every(digit => digit === cpfArray[0])
+  if (isCpfInvalid) {
+    return false;
+  else {
+     return digitValidator(cpfArray)
+  }
 }
 
 function digitValidator(cpf) {
-    let nineDigitsCpf = cpf.slice(0, 9);
-    let firstDigit = getDigits(nineDigitsCpf, false);
-    let tenDigitsCpf = nineDigitsCpf;
-    tenDigitsCpf.push(firstDigit);
-    let secondDigit = getDigits(tenDigitsCpf, true);
-    if (firstDigit === parseInt(cpf[9]) && secondDigit === parseInt(cpf[10])) {
-        return true;
-    } else {
-        return false;
-    }   
+    const nineDigitsCpf = cpf.slice(0, 9);
+    const firstDigit = getDigits(nineDigitsCpf, false);
+    const tenDigitsCpf = [...nineDigitsCpf, firstDigit];
+    const secondDigit = getDigits(tenDigitsCpf, true);
+    return firstDigit === parseInt(cpf[9]) && secondDigit === parseInt(cpf[10]); 
 }
 
 function getDigits(cpf, secondDigit) {
-    let number = secondDigit ? 11 : 10;
-    let multiply = cpf.map((element, index) => element * (number - index));
-    let sum = multiply.reduce((acc, element) => acc + element);
-    let digit = (sum % 11) < 2 ? 0 : 11 - (sum % 11);
-    return digit;
+    const number = secondDigit ? 11 : 10;
+    const multiply = cpf.map((element, index) => element * (number - index));
+    const sum = multiply.reduce((acc, element) => acc + element);
+    return (sum % 11) < 2 ? 0 : 11 - (sum % 11);
 }
 
-exports.cpf = cpf;
+exports.cpfValidator = cpfValidator;
